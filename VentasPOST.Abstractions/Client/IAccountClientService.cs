e9c635b42f20a3d1f635de50146c5f2364c03e36
ElPelfry <delfry_paulino@ucne.edu.do>
2024-12-24 01:06:34 -0400

    Agrega abstractions de cliente y de servidor

---
+++ VentasPOST.Abstractions/Client/IAccountClientService.cs
@@ -0,0 +1,16 @@
+﻿using VentasPOST.Domain.DTOs.Request.Account;
+using VentasPOST.Domain.DTOs.Response.Account;
+using VentasPOST.Domain.DTOs.Response;
+
+namespace VentasPOST.Abstractions.Client;
+
+public interface IAccountClientService
+{
+    Task CreateAdmin();
+    Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model);
+    Task<LoginResponse> LoginAccountAsync(LoginDTO model);
+    Task<LoginResponse> RefreshTokenAsync(RefreshTokenDTO model);
+    Task<IEnumerable<GetRoleDTO>> GetRolesAsync();
+    Task<IEnumerable<GetUsersWithRolesResponseDTO>> GetUsersWithRolesAsync();
+    Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model);
+}
---
+++ VentasPOST.Abstractions/Server/IAccountServerService.cs
@@ -0,0 +1,17 @@
+﻿using VentasPOST.Domain.DTOs.Request.Account;
+using VentasPOST.Domain.DTOs.Response.Account;
+using VentasPOST.Domain.DTOs.Response;
+
+namespace VentasPOST.Abstractions.Server;
+
+public interface IAccountServerService
+{
+    Task CreateAdmin();
+    Task<GeneralResponse> CreateAccountAsync(CreateAccountDTO model);
+    Task<LoginResponse> LoginAccountAsync(LoginDTO model);
+    Task<LoginResponse> RefreshTokenAsync(RefreshTokenDTO model);
+    Task<GeneralResponse> CreateRoleAsync(CreateRoleDTO model);
+    Task<IEnumerable<GetRoleDTO>> GetRolesAsync();
+    Task<IEnumerable<GetUsersWithRolesResponseDTO>> GetUsersWithRolesAsync();
+    Task<GeneralResponse> ChangeUserRoleAsync(ChangeUserRoleRequestDTO model);
+}
